9b2d3ecba368e50645bc1c00a481803be4ad08fe
Joseph Berryman <berryman.joseph4228@gmail.com>
2014-12-27 13:22:18 -0600

    placed all the lightbox images; need to insert the html for remaining gallery sets

--- psResize.jsx
+++ psResize.jsx
@@ -1,10 +1,15 @@
-var fileRef,docRef,savedState,savePath, filePath = "//vboxsrv/BrazConstruction/braz/img/g_venetian";
+app.preferences.rulerUnits = Units.PIXELS;
+var dir = "//vboxsrv/Windows7Ultimate_shrFldr/brz_images_0/",
+    folderObj = Folder(dir),
+    files = folderObj.getFiles("*"),
+    fileRef, docRef, strFileName, newFileName;
 
 
-for (var i = 3; i < 6; ++i){
-    fileRef = File(filePath + i + ".jpg");
+for (var i = 0; i < files.length; ++i){
+    fileRef = File(files[i].absoluteURI);
+    strFileName = fileRef.name;
+
     docRef = app.open(fileRef);
-
     savedState = docRef.activeHistoryState;
 
     jpgSaveOptions = new JPEGSaveOptions();
@@ -12,27 +17,24 @@ for (var i = 3; i < 6; ++i){
     jpgSaveOptions.matte = MatteType.NONE;
     jpgSaveOptions.quality = 12;
 
-    docRef.resizeImage(1000,750);
-    savePath = new File(filePath + i + "_1000x750.jpg");
-    app.activeDocument.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
+    docRef.resizeImage(UnitValue(1000,"px"),UnitValue(750,"px"));
+    newFileName = strFileName.replace(".jpg", "_1000x750.jpg");
+    savePath = new File(dir + newFileName);
+    docRef.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
     savePath = null;
     docRef.activeHistoryState = savedState;
 
-    docRef.resizeImage(800,600);
-    savePath = new File(filePath + i + "_800x600.jpg");
-    app.activeDocument.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
+    docRef.resizeImage(UnitValue(800,"px"),UnitValue(600,"px"));
+    newFileName = strFileName.replace(".jpg", "_800x600.jpg");
+    savePath = new File(dir + newFileName);
+    docRef.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
     savePath = null;
     docRef.activeHistoryState = savedState;
 
-    docRef.resizeImage(400,300);
-    savePath = new File(filePath + i + "_400x300.jpg");
-    app.activeDocument.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
-    savePath = null;
-    docRef.activeHistoryState = savedState;
-
-    docRef.resizeImage(250,188);
-    savePath = new File(filePath + i + "_250x188.jpg");
-    app.activeDocument.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
+    docRef.resizeImage(UnitValue(400,"px"),UnitValue(300,"px"));
+    newFileName = strFileName.replace(".jpg", "_400x300.jpg");
+    savePath = new File(dir + newFileName);
+    docRef.saveAs(savePath,jpgSaveOptions,true,Extension.LOWERCASE);
     savePath = null;
     docRef.activeHistoryState = savedState;
 
