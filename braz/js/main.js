0ddf13d3921074a8275035edd9ab8d12ff9dd0f4
Joseph Berryman <berryman.joseph4228@gmail.com>
2015-01-26 22:03:02 -0600

    wrapped preloader in .ready(); changed gallery bgs to DarkGrey for < 768; created Tall version of bgs for screens taller than they are wide; fixed nav to hide on item click; need to add latest images;

--- braz/js/main.js
+++ braz/js/main.js
@@ -11,20 +11,33 @@ $(document).ready(function(){
     gallImgDesc.css('bottom', position);
   });
 
+  $('.navbar-collapse').click('li', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
   if ($(window).width() < 768) {
   $(".backstretch-carousel").backstretch([
-    "./img/bg-contact.jpg",
-    "./img/bg-welcome_half.jpg",
-    "./img/bg-services_naked.jpg",
-    "./img/bg-gallery1.jpg",
-    "./img/bg-gallery2.jpg",
-    "./img/bg-gallery3.jpg"],
+    "./img/bg-contactHalf.jpg",
+    "./img/bg-welcomeHalf.jpg",
+    "./img/bg-servicesHalf.jpg",
+    "./img/bg-texture_DarkGrey_Red.jpg",
+    "./img/bg-texture_DarkGrey_Green.jpg",
+    "./img/bg-texture_DarkGrey_Beige.jpg"],
+    {duration:1000,fade:1000});
+  } else if ($(window).width() > 768 && ($(window).height() >= 2*$(window).width())) {
+    $(".backstretch-carousel").backstretch([
+    "./img/bg-contactTall.jpg",
+    "./img/bg-welcomeTall.jpg",
+    "./img/bg-servicesTall.jpg",
+    "./img/bg-texture_DarkGrey_Red.jpg",
+    "./img/bg-texture_DarkGrey_Green.jpg",
+    "./img/bg-texture_DarkGrey_Beige.jpg"],
     {duration:1000,fade:1000});
   } else {
     $(".backstretch-carousel").backstretch([
       "./img/bg-contact.jpg",
       "./img/bg-welcome.jpg",
-      "./img/bg-services_naked.jpg",
+      "./img/bg-services.jpg",
       "./img/bg-gallery1.jpg",
       "./img/bg-gallery2.jpg",
       "./img/bg-gallerry3.jpg"],
@@ -42,19 +55,21 @@ $(document).ready(function(){
     }
   }, { offset: '50%' });
 
-  $('.img-window').hover(function(event){
-      var img = $(this).children('.gall-image');
-      var imgSrc = img.attr("src");
-      var newSrc = imgSrc.replace(".png", "Tint.png");
-      img.attr("src", newSrc);
-      $(this).find('.gall-img-desc').addClass('animated fadeInDown').css('display', 'block');
-  },function(){
-      var img = $(this).children('.gall-image');
-      var imgSrc = img.attr("src");
-      var newSrc = imgSrc.replace("Tint.png" ,".png");
-      img.attr("src", newSrc);
-      $(this).find('.gall-img-desc').removeClass('animated fadeInDown').css('display', 'none');
-  });
+  if ($(window).width() >= 768){
+    $('.img-window').hover(function(event){
+        var img = $(this).children('.gall-image');
+        var imgSrc = img.attr("src");
+        var newSrc = imgSrc.replace(".png", "Tint.png");
+        img.attr("src", newSrc);
+        $(this).find('.gall-img-desc').addClass('animated fadeInDown').css('display', 'block');
+    },function(){
+        var img = $(this).children('.gall-image');
+        var imgSrc = img.attr("src");
+        var newSrc = imgSrc.replace("Tint.png" ,".png");
+        img.attr("src", newSrc);
+        $(this).find('.gall-img-desc').removeClass('animated fadeInDown').css('display', 'none');
+    });
+  }
 
   $('#services-content-container').tinyscrollbar({trackSize: 100, wheelLock: false});
   $('#contact-content-container').tinyscrollbar({trackSize: 100, wheelLock: false});
